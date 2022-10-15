a802a9bd26417f380159093c51e8a5a4e643f0a6
kazchrome <kristoffzimmerman@gmail.com>
2022-10-15 10:14:38 -0400

    finished 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let tempC = (tempF -32)*(5/9);
+  let roundTempC = Math.round (tempC * 10) / 10;
+  return roundTempC;
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let tempF = (tempC*(9/5))+32;
+  let roundTempF = Math.round (tempF * 10) / 10;
+  return roundTempF;
 };
 
 // Do not edit below this line
